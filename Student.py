59c4c82cdcf465c0f060e2907336d096941ff80f
grantkee <grant.d.kee@gmail.com>
2020-03-26 17:42:06 -0500

    removed erroneous code for now.

--- Student.py
+++ Student.py
@@ -1,11 +1,11 @@
 class Student:
   def __init__(self, name, major, major_id, gpa):
     self.name = name
-    self.major_id = major_id
     self.major = major
+    self.major_id = major_id
     self.gpa = gpa
 
-  ids: ["STEM", "The Arts", "Business", "English"]
+  # ids: ["STEM", "Art", "Business", "English"]
 
   def on_honor_roll(self):
     if self.gpa >= 3.5:
@@ -13,7 +13,7 @@ class Student:
     else:
       return False
 
-  def field_of_study(self, ids):
-    if self.major_id in ids
-      return self.name
+  # def field_of_study(self, ids):
+  #   if self.major_id in ids
+  #     return self.name
     
--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from Student import Student
 
-student1 = Student("Monique", "Finance", 3.8)
-student2 = Student("Zeus", "Chemistry", 3.3)
-student3 = Student("Cynthia", "Art", 2.4)
-student4 = Student("Ross", "Music", 3.6)
-student5 = Student("Abigail", "English", 3.5)
-student6 = Student("Geromy", "Math", 2.9)
-student7 = Student("Delila", "Biology", 3.1)
-student8 = Student("Monica", "Physics", 3.7)
-student9 = Student("Larry", "Comedy", 3.5)
-student10 = Student("Samantha", "Rhetoric", 2.2)
+student1 = Student("Monique", "Finance", "Business", 3.8)
+student2 = Student("Zeus", "Chemistry", "STEM", 3.3)
+student3 = Student("Cynthia", "Art History", "Art", 2.4)
+student4 = Student("Ross", "Music", "Art", 3.6)
+student5 = Student("Abigail", "English", "English", 3.5)
+student6 = Student("Geromy", "Math", "STEM", 2.9)
+student7 = Student("Delila", "Biology", "STEM", 3.1)
+student8 = Student("Monica", "Physics", "STEM", 3.7)
+student9 = Student("Larry", "Comedy", "Art", 3.5)
+student10 = Student("Samantha", "Rhetoric", "English", 2.2)
+student11 = Student("Bob", "Writing", "English", 3.9)
 
 students = [student1, student2, student3, student4, student5, student6]
 results = map(Student.on_honor_roll, students)
